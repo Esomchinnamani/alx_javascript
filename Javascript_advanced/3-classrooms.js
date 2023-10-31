cc553f0236541532ee66f9a0f1190e1bd17e7ca7
Esomchinoel <esomedu@gmail.com>
2023-10-31 13:32:31 +0100

    hotfix

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        };
+    }
+
+    let students = [];
+
+    for (let i = 1; i <= numberOfStudents; i++) {
+        students.push(studentSeat(i));
+    }
+    return students;
+    
+    let classRoom = createClassRoom(10);
+}
