47bbbedbf8bdc24c392bed7598aa35f0fbc2b6d8
Esomchinoel <esomedu@gmail.com>
2023-10-31 16:04:34 +0100

    fix

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,43 +1,23 @@
+#!/usr/bin/node
 const request = require('request');
 
 const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
-if (!movieId) {
-  console.error('Usage: node starwars_characters.js <MOVIE_ID>');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  const movieData = JSON.parse(body);
-
-  console.log(`Characters in ${movieData.title}:`);
-
-  movieData.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
+request(url + movieId, (error, response, body) => {
+    if (error) {
+        console.log(error);
         return;
-      }
+    }
+    const movies = JSON.parse(body);
 
-      if (charResponse.statusCode !== 200) {
-        console.error(`Request failed with status code ${charResponse.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
+    for (const movie of movies.characters) {
+        request.get(movie, (error, response, body1) => {
+            if (error) {
+                console.log(error);
+            }
+            const movieData = JSON.parse(body1);
+            console.log(movieData.name);
+        });
+    }
 });
