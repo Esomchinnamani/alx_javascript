91a40a67e4fe5448c8b17e6a06e8440edd375061
Esomchinoel <esomedu@gmail.com>
2023-10-31 13:20:15 +0100

    fix

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const myObject = {
+    type: 'object',
+    value: 12,
+    incr: function () {
+      this.value++;
+    },
+    inspect: function () {
+      return `{ type: '${this.type}', value: ${this.value}${this.incr ? `, incr: [Function]` : ''} }`;
+    },
+  };
+  
+  console.log(myObject);
+  
